7717979ad0348be708430379f371985aaee91506
AYUSH AGARWAL <ayush.agarwal@rentomojo.com>
2019-08-12 20:24:59 +0530

    Initialized repository

---
+++ index.js
@@ -0,0 +1,14 @@
+(function() {
+        if(sessionStorage.getItem("username")) {
+            window.location.href = `./game.html`;
+        }
+        const userNameForm = document.getElementById("usernameform");
+        let userName = "";
+        userNameForm.addEventListener("submit", (e) => {
+            e.preventDefault();
+            userName = userNameForm.elements['username'].value;
+            sessionStorage.setItem('username', userName);
+            window.location.href = `./game.html`;
+        });
+    }
+)();
