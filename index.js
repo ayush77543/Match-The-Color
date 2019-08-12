c45733540c3e43f231dddc82c5f8c16c0b16589a
AYUSH AGARWAL <ayush.agarwal@rentomojo.com>
2019-08-12 20:28:47 +0530

    Ran Js lint

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 (function() {
-        if(sessionStorage.getItem("username")) {
-            window.location.href = `./game.html`;
-        }
-        const userNameForm = document.getElementById("usernameform");
-        let userName = "";
-        userNameForm.addEventListener("submit", (e) => {
-            e.preventDefault();
-            userName = userNameForm.elements['username'].value;
-            sessionStorage.setItem('username', userName);
-            window.location.href = `./game.html`;
-        });
+    if (sessionStorage.getItem("username")) {
+        window.location.href = `./game.html`;
     }
-)();
+    const userNameForm = document.getElementById("usernameform");
+    let userName = "";
+    userNameForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        userName = userNameForm.elements['username'].value;
+        sessionStorage.setItem('username', userName);
+        window.location.href = `./game.html`;
+    });
+})();
